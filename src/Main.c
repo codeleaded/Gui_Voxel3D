e4dd7d110c2927db94218816faec7dcd04da355c
codeleaded <codeleaded@gmail.com>
2025-11-20 15:11:53 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -28,19 +28,18 @@ void Menu_Set(int m){
 }
 
 void Setup(AlxWindow* w){
-	Menu_Set(1);
+	Menu_Set(0);
 
 	RGA_Set(Time_Nano());
 	RGA_Get(6969);
 	PerlinNoise_Permutations_Init();
 
 	voxelworld = VoxelWorld_New(
-		Camera_Make(
+		Figure3D_New(
 			Vec3D_New(10.0f,100.0f,10.0f),
-			Vec3D_New(0.5f,1.8f,0.5f),
+			Vec3D_New(0.5f,1.4f,0.5f),
 			Vec3D_New(0.0f,-10.0f,0.0f),
-			Vec3D_New(0.0f,0.0f,0.0f),
-			90.0f
+			1
 		),
 		"../data"
 	);
@@ -53,9 +52,9 @@ void Update(AlxWindow* w){
 			Vec2 d = Vec2_Sub(GetMouse(),voxelworld.mousebefore);
 			Vec2 a = Vec2_Mulf(Vec2_Div(d,(Vec2){ window.Width,window.Height }),F32_PI2);
 	
-			Camera* cam = VoxelWorld_GetCam(&voxelworld);
-			cam->a.y += a.x;
-			cam->a.x += a.y;
+			Figure3D* fig = VoxelWorld_GetFigure(&voxelworld);
+			fig->a.y += a.x;
+			fig->a.x += a.y;
 	
 			SetMouse((Vec2){ GetWidth() / 2,GetHeight() / 2 });
 			voxelworld.mousebefore = GetMouse();
@@ -68,24 +67,33 @@ void Update(AlxWindow* w){
 	if(Stroke(ALX_KEY_Z).PRESSED)
 		voxelworld.mode = voxelworld.mode < 3 ? voxelworld.mode + 1 : 0;
 
-	if(Stroke(ALX_KEY_R).DOWN) 		Camera_AccUp(VoxelWorld_GetCam(&voxelworld),5.0f);
-	else if(Stroke(ALX_KEY_F).DOWN) Camera_AccUp(VoxelWorld_GetCam(&voxelworld),-5.0f);
-	else							Camera_AccUp(VoxelWorld_GetCam(&voxelworld),0.0f);
-	
-	if(Stroke(ALX_KEY_W).DOWN) 	Camera_AccForward(VoxelWorld_GetCam(&voxelworld),20.0f * w->ElapsedTime);
-	if(Stroke(ALX_KEY_S).DOWN) 	Camera_AccForward(VoxelWorld_GetCam(&voxelworld),-20.0f * w->ElapsedTime);
-	if(Stroke(ALX_KEY_A).DOWN) 	Camera_AccLeft(VoxelWorld_GetCam(&voxelworld),20.0f * w->ElapsedTime);
-	if(Stroke(ALX_KEY_D).DOWN)  Camera_AccLeft(VoxelWorld_GetCam(&voxelworld),-20.0f * w->ElapsedTime);
+	if(Stroke(ALX_KEY_C).PRESSED){
+		Figure3D* fig = VoxelWorld_GetFigure(&voxelworld);
+		fig->OnView = fig->OnView < 3 ? fig->OnView + 1 : 0;
+	}
+	if(Stroke(ALX_KEY_B).PRESSED){
+		Figure3D* fig = VoxelWorld_GetFigure(&voxelworld);
+		fig->OnCollision = fig->OnCollision < 1 ? fig->OnCollision + 1 : 0;
+	}
 
-	if(Stroke(ALX_MOUSE_L).PRESSED) VoxelWorld_Camera_Place(&voxelworld,BLOCK_VOID,4.0f);
-	if(Stroke(ALX_MOUSE_R).PRESSED) VoxelWorld_Camera_PlaceN(&voxelworld,BLOCK_TORCH,4.0f);
+	if(Stroke(ALX_KEY_R).DOWN) 		Figure3D_AccUp(VoxelWorld_GetFigure(&voxelworld),5.0f);
+	else if(Stroke(ALX_KEY_F).DOWN) Figure3D_AccUp(VoxelWorld_GetFigure(&voxelworld),-5.0f);
+	else							Figure3D_AccUp(VoxelWorld_GetFigure(&voxelworld),0.0f);
+	
+	if(Stroke(ALX_KEY_W).DOWN) 	Figure3D_AccForward(VoxelWorld_GetFigure(&voxelworld),20.0f * w->ElapsedTime);
+	if(Stroke(ALX_KEY_S).DOWN) 	Figure3D_AccForward(VoxelWorld_GetFigure(&voxelworld),-20.0f * w->ElapsedTime);
+	if(Stroke(ALX_KEY_A).DOWN) 	Figure3D_AccLeft(VoxelWorld_GetFigure(&voxelworld),20.0f * w->ElapsedTime);
+	if(Stroke(ALX_KEY_D).DOWN)  Figure3D_AccLeft(VoxelWorld_GetFigure(&voxelworld),-20.0f * w->ElapsedTime);
+
+	if(Stroke(ALX_MOUSE_L).PRESSED) VoxelWorld_Place(&voxelworld,BLOCK_VOID,4.0f);
+	if(Stroke(ALX_MOUSE_R).PRESSED) VoxelWorld_PlaceN(&voxelworld,BLOCK_TORCH,4.0f);
 
 	VoxelWorld_Update(&voxelworld,w->ElapsedTime);
 
 	Clear(LIGHT_BLUE);
 	VoxelWorld_Render(WINDOW_STD_ARGS,&voxelworld);
 
-	String str = String_Format("X: %f, Y: %f, Z: %f, Size: %d",VoxelWorld_GetCam(&voxelworld)->p.x,VoxelWorld_GetCam(&voxelworld)->p.y,VoxelWorld_GetCam(&voxelworld)->p.z,voxelworld.meshselected->size);
+	String str = String_Format("X: %f, Y: %f, Z: %f, Size: %d",VoxelWorld_GetFigure(&voxelworld)->p.x,VoxelWorld_GetFigure(&voxelworld)->p.y,VoxelWorld_GetFigure(&voxelworld)->p.z,voxelworld.meshselected->size);
 	RenderCStrSize(str.Memory,str.size,0,0,RED);
 	String_Free(&str);
 }
